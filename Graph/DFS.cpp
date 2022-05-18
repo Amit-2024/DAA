356b174bcbb0e60cfeab8b773e18383f085b8a01
Amit Semwal <100823495+Amit-2024@users.noreply.github.com>
2022-05-18 20:42:43 +0530

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void addEdge(vector<int> a[], int u, int v)
+void addEdgeU(vector<int> a[], int u, int v)
 {
     a[u].push_back(v);
     a[v].push_back(u);
+}
+
+void addEdgeD(vector<int> a[], int u, int v)
+{
+    a[u].push_back(v);
 }
 
 void dfs(vector<int> a[], int u, bool visited[])
@@ -31,11 +36,25 @@ int main()
 
     vector<int> a[n];
 
-    addEdge(a, 0, 1);
-    addEdge(a, 1, 2);
-    addEdge(a, 2, 3);
-    addEdge(a, 3, 4);
+    int x;
+    cout << "--FOR Undirect press 1 else press 2--- : ";
+    cin >> x;
 
+    if (x == 1)
+    {
+        addEdgeU(a, 0, 1);
+        addEdgeU(a, 1, 2);
+        addEdgeU(a, 2, 3);
+        addEdgeU(a, 3, 4);
+    }
+
+    else if (x == 2)
+    {
+        addEdgeD(a, 0, 1);
+        addEdgeD(a, 1, 2);
+        addEdgeD(a, 2, 3);
+        addEdgeD(a, 3, 4);
+    }
     bool visited[n];
 
     for (int i = 0; i < n; i++)
